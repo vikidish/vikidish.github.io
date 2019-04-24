f3a96fcfdf8c8e88622c5aa071dbbcc8a5f76b6f
vikidish <30635610+vikidish@users.noreply.github.com>
2019-04-24 11:47:21 +0300

    1st

---
+++ lists/precache-manifest.0709f84917ef44624191c11041e690a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d023cb007e3f6e175d2fea3470852f2",
+    "url": "/lists/index.html"
+  },
+  {
+    "revision": "b75ddb4dce65b7d0ef52",
+    "url": "/lists/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/lists/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "b75ddb4dce65b7d0ef52",
+    "url": "/lists/static/js/main.824c7129.chunk.js"
+  },
+  {
+    "revision": "1772854de5bbddb3b56f",
+    "url": "/lists/static/js/runtime~main.8cf9636b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/lists/static/media/logo.5d5d9eef.svg"
+  }
+]);
